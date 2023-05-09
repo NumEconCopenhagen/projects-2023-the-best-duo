2a745fd963aa56517e01766d473ebd79c2454fe0
djf698 <djf698@alumni.ku.dk>
2023-05-09 10:51:49 +0200

    Something is happening which is good i guess

--- modelproject/class_1.py
+++ modelproject/class_1.py
@@ -7,11 +7,15 @@ import itertools
 
 class buyer:
     def __init__(self, id, max_p, curr_d_p):
+        # identification
         self.id = id
+
+        
         self.max_p = max_p
         self.curr_p = curr_d_p
         self.curr_surplus = 0
         self.bought = False
+
 
 class seller:
     def __init__(self, id, min_p, curr_s_p):
@@ -23,7 +27,7 @@ class seller:
 
 class economy_simulation:
 
-    def __init__(self, dem_func, sup_func, initial_p, dem_dom=[0,1], sup_dom=[0,1], n_days=30, n_buyers=25, n_sellers=25, ):
+    def __init__(self, dem_func, sup_func, initial_p, n_days=30, n_buyers=25, n_sellers=25, ):
         """Creates an economy simulation
         
         Parameters:
@@ -43,8 +47,6 @@ class economy_simulation:
         par.n_sellers = n_sellers
         par.dem_func = dem_func
         par.sup_func = sup_func
-        par.dem_dom = dem_dom
-        par.sup_dom = sup_dom
         par.initial_p = initial_p
     
     def simulate(self):
@@ -54,34 +56,49 @@ class economy_simulation:
         
 
         # b. simulations
-        buyers = [buyer(i,par.dem_func(i),par.initial_p) for i in np.linspace(par.dem_dom[0],par.dem_dom[1], par.n_buyers)]
-        sellers = [seller(i,par.sup_func(i),par.initial_p) for i in np.linspace(par.sup_dom[0],par.sup_dom[1], par.n_sellers)]
+        buyers = [buyer(i,par.dem_func(i+1),par.initial_p) for i in range(par.n_buyers)]
+        sellers = [seller(i,par.sup_func(i+1),par.initial_p) for i in range(par.n_sellers)]
         poss_comb = list(itertools.product(buyers,sellers))
-        for i, comb in enumerate(poss_comb):
-            poss_comb[i] = list(comb)
+        poss_comb = [list(comb) for comb in poss_comb]
 
         iter = 0
 
         for day in range(par.n_days):
             rand.shuffle(poss_comb)
 
-            print("New day")
+            print(f'day {day}:')
             print(poss_comb)
             for chosen_pair in poss_comb:
                 
                 # were choosing one interaction
                 chosen_buyer, chosen_seller = chosen_pair
                 
-                p = trade(chosen_buyer,chosen_seller,par.n_sellers)
+                p = trade(chosen_buyer,chosen_seller,par.n_sellers,do_print=True)
 
-                print(p)
+            # reset
+            list_agents = []
+            for chosen_pair in poss_comb:
+                chosen_buyer, chosen_seller = chosen_pair
+                
+                if chosen_buyer not in list_agents:
+                    list_agents.append(chosen_buyer)
+                    if not chosen_buyer.bought:
+                        chosen_buyer.curr_p += 1
+                    else:
+                        chosen_buyer.bought = False
+
+                if chosen_seller not in list_agents:
+                    list_agents.append(chosen_seller)
+                    if not chosen_seller.sold:
+                        chosen_seller.curr_p -= 1
+                    else:
+                        chosen_seller.sold = False
+                
+            
                 
                 
             
-            # reset
-            for i in len(poss_comb):
-                poss_comb[i][0].bought = False
-                poss_comb[i][1].sold = False
+            
 
             
         
