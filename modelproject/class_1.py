3ea927b749ea024a9344b95f0084ef080ac5436a
djf698 <djf698@alumni.ku.dk>
2023-05-11 20:50:09 +0200

    OMG LOOK AT THOSE CURRRVES

--- modelproject/class_1.py
+++ modelproject/class_1.py
@@ -6,7 +6,7 @@ import matplotlib.pyplot as plt
 import itertools
 
 class agent:
-    def __init__(self, id, max_p, curr_d_p, min_rw, curr_s_w,initial_cash=1000):
+    def __init__(self, id, max_p, curr_d_p, min_rw, curr_s_w):
         # identification
         self.id = id
 
@@ -22,13 +22,10 @@ class agent:
         self.rw_curr_surplus = 1
         self.worked = False
 
-        # constraints
-        self.cash = initial_cash
-
 
 
 class firm:
-    def __init__(self, id, min_p, curr_s_p, max_rw, curr_d_w,initial_cash=1000):
+    def __init__(self, id, min_p, curr_s_p, max_rw, curr_d_w):
         # identification
         self.id = id
 
@@ -44,13 +41,9 @@ class firm:
         self.rw_curr_surplus = 0
         self.employed = False
 
-        # constraints
-        self.cash = initial_cash
-        self.stock = 0
-
 class economy_simulation:
 
-    def __init__(self, p_dem_func, p_sup_func,initial_p, rw_dem_func, rw_sup_func, initial_w , n_days=30, n_agents=25, n_firms=25):
+    def __init__(self, p_dem_func=None, p_sup_func=None,initial_p=None, rw_dem_func=None, rw_sup_func=None, initial_w=None , n_days=30, n_agents=25, n_firms=25,only_goods=False,only_labor=False):
         """Creates an economy simulation
         
         Parameters:
@@ -60,7 +53,7 @@ class economy_simulation:
         sup_dom: [left_bound, right_bound] it's the domain of the supply function
         
         """
-
+        assert (not only_goods or not only_labor)
 
         par = self.par = SimpleNamespace()
         
@@ -70,16 +63,29 @@ class economy_simulation:
         par.n_firms = n_firms
 
         # good market parameters
-        par.p_dem_func = p_dem_func
-        par.p_sup_func = p_sup_func
-        par.initial_p = initial_p
+        if only_labor:
+            par.p_dem_func = lambda x: 1
+            par.p_sup_func = lambda x: 1
+            par.initial_p = initial_p
+        else:
+            par.p_dem_func = p_dem_func
+            par.p_sup_func = p_sup_func
+            par.initial_p = initial_p
+        
+        par.only_goods = only_goods
 
         # labor market parameters
-        par.rw_dem_func = rw_dem_func
-        par.rw_sup_func = rw_sup_func
-        par.initial_w = initial_w
+        if only_goods:
+            par.rw_dem_func = lambda x: 1
+            par.rw_sup_func = lambda x: 1
+            par.initial_w = 1
+        else:
+            par.rw_dem_func = rw_dem_func
+            par.rw_sup_func = rw_sup_func
+            par.initial_w = initial_w
+        par.only_labor = only_labor
     
-    def simulation(self,constrained=False):
+    def simulation(self):
 
         # a. setup
         par = self.par
@@ -87,108 +93,146 @@ class economy_simulation:
 
         # b. simulations
         agents = [agent(i,par.p_dem_func(i+1),par.initial_p,par.rw_sup_func(i+1),par.initial_w) for i in range(par.n_agents)]
-        firms = [firm(i,par.p_sup_func(i+1),par.initial_p,par.rw_sup_func(i+1),par.initial_w) for i in range(par.n_firms)]
+        firms = [firm(i,par.p_sup_func(i+1),par.initial_p,par.rw_dem_func(i+1),par.initial_w) for i in range(par.n_firms)]
         poss_comb = list(itertools.product(agents,firms))
         poss_comb = [list(comb) for comb in poss_comb]
         
         # accounting
         inflation_list = []
         wage_list = []
+        g_demand_list = []
+        g_supply_list = []
+        l_demand_list = []
+        l_supply_list = []
 
         # day simulation
         for day in range(par.n_days):
             print(f'day {day}:')
-            day_w_list = []
-            # labor market 
-            rand.shuffle(poss_comb)
-            for chosen_pair in poss_comb:
-                # were choosing one interaction
-                chosen_worker, chosen_employer = chosen_pair
-                
-                w = labor_market(chosen_worker,chosen_employer,inflation,constrained=constrained)
+            
+            l_Supply = 0
+            l_Demand = 0
 
-                if str(w) != "nan":
-                    day_w_list.append(w)
-            
-            if day_w_list != []:
-                w = sum(day_w_list)/len(day_w_list)
-            
-            wage_list.append(w)
-            
-            list_agents = []
-            for chosen_pair in poss_comb:
-                chosen_worker, chosen_employer = chosen_pair
+            if not par.only_goods:
+                day_w_list = []
+                # labor market 
+                rand.shuffle(poss_comb)
+                for chosen_pair in poss_comb:
+                    # were choosing one interaction
+                    chosen_worker, chosen_employer = chosen_pair
+                    inter_w = labor_market(chosen_worker,chosen_employer,inflation,do_print=True)
+
+                    if str(inter_w) != "nan":
+                        day_w_list.append(inter_w)
                 
-                if chosen_worker not in list_agents:
-                    list_agents.append(chosen_worker)
-                    if not chosen_worker.worked:
-                        chosen_worker.curr_w -= 1
+                if day_w_list != []:
+                    day_w = sum(day_w_list)/len(day_w_list)
+                else:
+                    day_w = np.nan
+                
+                # clearing up all the nans
+                if str(day_w) == "nan":
+                    if day == 0:
+                        day_w = par.initial_w
                     else:
-                        chosen_worker.worked = False
+                        day_w = wage_list[-1]
 
-                if chosen_employer not in list_agents:
-                    list_agents.append(chosen_employer)
-                    if not chosen_employer.employed:
-                        chosen_employer.curr_w += 1
-                    else:
-                        chosen_employer.employed = False
-
-
-
-
-            # good market
-            rand.shuffle(poss_comb)
-            p_list = []
-            
-            
-            for chosen_pair in poss_comb:
+                wage_list.append(day_w)
                 
-                # were choosing one interaction
-                chosen_buyer, chosen_seller = chosen_pair
-                
-                p = trade(chosen_buyer,chosen_seller,do_print=True)
-                
-                if str(p) != "nan":
-                    p_list.append(p)
-            
-            if p_list != []:
-                inflation = sum(p_list)/len(p_list)
-
-            print("today's price:",inflation,"Today's wage:",w,"real wage:",w/inflation)
-            Demand = 0
-            Supply = 0
-
-            # reset
-            list_agents = []
-            for chosen_pair in poss_comb:
-                chosen_buyer, chosen_seller = chosen_pair
-                
-                if chosen_buyer not in list_agents:
-                    list_agents.append(chosen_buyer)
+                list_agents = []
+                for chosen_pair in poss_comb:
+                    chosen_worker, chosen_employer = chosen_pair
                     
-                    if not chosen_buyer.bought:
-                        chosen_buyer.curr_p += 1
+                    if chosen_worker not in list_agents:
+                        list_agents.append(chosen_worker)
+                        if not chosen_worker.worked:
+                            chosen_worker.curr_w -= 1
+                        else:
+                            chosen_worker.worked = False
+                        if day_w > int(chosen_worker.min_rw*inflation)+1:
+                            l_Supply += 1
 
-                    else:
-                        chosen_buyer.bought = False
-                    if inflation <= chosen_buyer.max_p:
-                        Demand += 1
+                    if chosen_employer not in list_agents:
+                        list_agents.append(chosen_employer)
+                        if not chosen_employer.employed:
+                            chosen_employer.curr_w += 1
+                        else:
+                            chosen_employer.employed = False
+                        if day_w < int(chosen_employer.max_rw*inflation):
+                            l_Demand += 1
                 
-                if chosen_seller not in list_agents:
-                    list_agents.append(chosen_seller)
-                    print(chosen_seller.cash)
-                    if not chosen_seller.sold:
-                        chosen_seller.curr_p -= 1
-                    else:
-                        chosen_seller.sold = False
-                    if inflation >= chosen_seller.min_p:
-                        Supply += 1
-            print(Demand,Supply)
+                l_demand_list.append(l_Demand)
+                l_supply_list.append(l_Supply)
 
-            inflation_list.append(inflation)
-            
-        
-        return inflation_list, wage_list
+
+            if not par.only_labor:
+                # good market
+                rand.shuffle(poss_comb)
+                p_list = []
+                
+                
+                for chosen_pair in poss_comb:
+                    
+                    # were choosing one interaction
+                    chosen_buyer, chosen_seller = chosen_pair
+                    
+                    p = trade(chosen_buyer,chosen_seller)
+                    
+                    if str(p) != "nan":
+                        p_list.append(p)
+                
+                if p_list != []:
+                    inflation = sum(p_list)/len(p_list)
+
+                
+                g_Demand = 0
+                g_Supply = 0
+
+                # reset
+                list_agents = []
+                for chosen_pair in poss_comb:
+                    chosen_buyer, chosen_seller = chosen_pair
+                    
+                    if chosen_buyer not in list_agents:
+                        list_agents.append(chosen_buyer)
+                        
+                        if not chosen_buyer.bought:
+                            chosen_buyer.curr_p += 1
+
+                        else:
+                            chosen_buyer.bought = False
+                        if inflation < chosen_buyer.max_p:
+                            g_Demand += 1
+                    
+                    if chosen_seller not in list_agents:
+                        list_agents.append(chosen_seller)
+                        if not chosen_seller.sold:
+                            chosen_seller.curr_p -= 1
+                        else:
+                            chosen_seller.sold = False
+                        if inflation > chosen_seller.min_p:
+                            g_Supply += 1
+
+                inflation_list.append(inflation)
+                g_demand_list.append(g_Demand)
+                g_supply_list.append(g_Supply)
+                
+                ending_phrase = ""
+                
+                if not par.only_labor:
+                    ending_phrase += f"Today's price: {inflation}, D: {g_Demand}, S: {g_Supply}"
+                    if not par.only_goods:
+                        ending_phrase += ", "
+                if not par.only_goods:
+                    ending_phrase += f"Today's wage: {day_w}, Real wage: {day_w/inflation}, D: {l_Demand}, S: {l_Supply}"
+
+                print(ending_phrase)
+                
+        if par.only_goods:
+            return inflation_list, g_demand_list, g_supply_list
+        elif par.only_labor:
+            return wage_list, l_demand_list, l_supply_list
+        else:
+            return inflation_list,g_demand_list, g_supply_list, wage_list, l_demand_list, l_supply_list
 
             # take out after
             
--- modelproject/functions_1.py
+++ modelproject/functions_1.py
@@ -1,37 +1,27 @@
 import numpy as np
 
-def trade(buyer,seller,constrained=False,do_print=False):
+def trade(buyer,seller,do_print=False):
     max_p = buyer.max_p
     min_p = seller.min_p
         
-    p_buyer = min(buyer.curr_p,max_p,buyer.cash)
+    p_buyer = min(buyer.curr_p,max_p)
     buyer.curr_p = p_buyer
     p_seller = max(seller.curr_p,min_p)
     seller.curr_p = p_seller
     p = np.nan
 
     # a. successful trade
-    if (constrained and seller.stock > 0 and buyer.cash >= p_seller) or not constrained:
-        if p_seller <= p_buyer and not seller.sold and not buyer.bought:
-            p = p_seller
-            buyer.p_curr_surplus = max_p - p_seller
-            seller.p_curr_surplus = p_seller - min_p
+    
+    if p_seller <= p_buyer and not seller.sold and not buyer.bought:
+        p = p_seller
+        buyer.p_curr_surplus = max_p - p_seller
+        seller.p_curr_surplus = p_seller - min_p
 
-            #i. price adjustments (agent decreases and firm increases)
-            buyer.curr_p -= 1
-            seller.curr_p += 1
-            buyer.bought = True
-            seller.sold = True
-
-            if constrained:
-                # constraints adjustments
-                buyer.cash -= p
-                seller.cash += p
-                seller.stock -= 1
-
-    else:
-        buyer.curr_w += 1
-        
+        #i. price adjustments (agent decreases and firm increases)
+        buyer.curr_p -= 1
+        seller.curr_p += 1
+        buyer.bought = True
+        seller.sold = True
     
     if do_print:
         if p is not np.nan:
@@ -41,7 +31,8 @@ def trade(buyer,seller,constrained=False,do_print=False):
     return p
 
 
-def labor_market(worker,employer,inflation,APL=2,constrained=False,do_print=False):
+def labor_market(worker,employer,inflation,do_print=False):
+    
     max_rw = employer.max_rw
     min_rw = worker.min_rw
     w_worker = max(worker.curr_w, int(min_rw*inflation)+1)
@@ -49,25 +40,20 @@ def labor_market(worker,employer,inflation,APL=2,constrained=False,do_print=Fals
     w_employer = min(employer.curr_w, int(max_rw*inflation))
     employer.curr_w = w_employer
     w = np.nan
-    if (constrained and employer.cash >= w_employer) or not constrained:
-        if  w_worker <= w_employer and not employer.employed and not worker.worked:
-            w = w_employer
-            worker.rw_curr_surplus = w - min_rw*inflation
-            employer.rw_curr_surplus = max_rw*inflation - w
 
-            #i. wage adjustments (agent increases and firm decreases)
-            employer.curr_w -= 1
-            worker.curr_w += 1
-            employer.employed = True
-            worker.worked = True
 
-            if constrained:
-                # constraints adjustments
-                employer.cash -= w
-                worker.cash += w
-                employer.stock += APL
-    else:
-        employer.curr_p += 1
+    if  w_worker <= w_employer and not employer.employed and not worker.worked:
+        w = w_employer
+        worker.rw_curr_surplus = w - min_rw*inflation
+        employer.rw_curr_surplus = max_rw*inflation - w
+
+        #i. wage adjustments (agent increases and firm decreases)
+        employer.curr_w -= 1
+        worker.curr_w += 1
+        employer.employed = True
+        worker.worked = True
+
+
 
     if do_print:
         if w is not np.nan:
