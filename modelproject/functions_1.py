700974b532dc1972485ac7efe0872be6836b1f3a
djf698 <djf698@alumni.ku.dk>
2023-05-12 20:48:35 +0200

    SOME BEAUTIFICATION GOIN ON

--- modelproject/functions_1.py
+++ modelproject/functions_1.py
@@ -4,6 +4,9 @@ import ipywidgets as widgets
 from class_1 import economy_simulation
 
 def trade(buyer,seller,do_print=False):
+    """
+    Will make an interaction between a buyer and a seller in the goods market.
+    """
     
     # a. absolute boundaries of price
     max_p = buyer.max_p
@@ -43,7 +46,10 @@ def trade(buyer,seller,do_print=False):
 
 
 def labor_market(worker,employer,inflation,do_print=False):
-    
+    """
+    Will make an interaction between a worker and an employer in the labor market.
+    """
+
     # a. absolute boundaries of real wage
     max_rw = employer.max_rw
     min_rw = worker.min_rw
@@ -81,7 +87,11 @@ def labor_market(worker,employer,inflation,do_print=False):
     return w
 
 def plot_func(p_dem_func=None, p_sup_func=None,initial_p=None, rw_dem_func=None, rw_sup_func=None, initial_w=None , n_days=30, n_persons=25, n_firms=25,only_goods=False,only_labor=False):
-    
+    """
+    Will plot the desired markets' results according to our model.
+    It has the same inputs as the class economy_simulation
+    """
+
     # a. simulate economy
     par = economy_simulation(p_dem_func, p_sup_func,initial_p, rw_dem_func, rw_sup_func, initial_w, n_days, n_persons, n_firms,only_goods,only_labor).simulation()
     
@@ -161,8 +171,19 @@ def plot_func(p_dem_func=None, p_sup_func=None,initial_p=None, rw_dem_func=None,
     plt.show()
 
 def stand_func(ab, n_persons=None, n_firms=None, dem=False, sup=False):
+    """
+    Creates a function based on a standardized function.
+    The standart function is f(x)=ax+b, where the domain is [0,1].
+    Then, it will stretch the function to accomodate for all the agents, either [0,n_persons] or [0,n_firms]
+    
+    A valid standardized demand function must have f(0)>0 and 0<f(1)<f(0), therefore b>0 and 0<b+a<b.
+    A valid standardized supply function must have f(0)>0 and f(1)>f(0), therefore b>0 and a+b>b.
+    """
+
+    # a. unpack the slope and intercept
     a, b = ab
     
+    # b. choose the right scalar
     if type(n_persons) == int:
         n = n_persons
     elif type(n_firms) == int:
@@ -170,40 +191,54 @@ def stand_func(ab, n_persons=None, n_firms=None, dem=False, sup=False):
     else:
         raise KeyError("n_firms or n_persons must contain an intenger")
 
+    # c. condition for a good demand curve 
     if dem:
         assert a < 0 and b > -a
-        
+    
+    # d. condition for a good supply curve
     if sup:
         assert a > 0 and b > 0
 
-    def standardized_function(x):
+    # e. create the function, by using the scalar on the standardized function
+    def function(x):
         return a*x/n + b
     
-    return standardized_function
+    # f. output
+    return function
 
-def update_x(p_dem,p_sup,l_dem,l_sup,n_persons,n_firms,initial_p, initial_w, n_days):
+def create_curves(p_dem,p_sup,l_dem,l_sup,n_persons,n_firms,initial_p, initial_w, n_days):
+    """
+    Will create all the curves and simulate them.
+    """
+    # a. create all functions
     p_dem_func = stand_func(p_dem, n_persons=n_persons, dem=True)
     p_sup_func = stand_func(p_sup, n_firms=n_firms, sup=True) 
     l_dem_func = stand_func(l_dem, n_firms=n_firms, dem=True)
     l_sup_func = stand_func(l_sup, n_persons=n_persons, sup=True)
 
+    # b. plot the results of the simulation
     plot_func(p_dem_func=p_dem_func, p_sup_func=p_sup_func,initial_p=initial_p, rw_dem_func=l_dem_func, rw_sup_func=l_sup_func, initial_w=initial_w , n_days=n_days, n_persons=n_persons, n_firms=n_firms,only_goods=False,only_labor=False)
 
 
 def interact():
-    
+    """
+    Will make an interactive plot.
+    """
+    # a. create dropdown for the curves
     p_dem = widgets.Dropdown(options=[("steeper",(-80,90)),("normal",(-50,75)),("flatter",(-20,60))],value=(-50,75))
     p_sup = widgets.Dropdown(options=[("steeper",(80,10)),("normal",(50,25)),("flatter",(20,40))],value=(50,25))
     l_dem = widgets.Dropdown(options=[("steeper",(-1.8,1.9)),("normal",(-1,1.5)),("flatter",(-0.2,1.1))],value=(-1,1.5))
     l_sup = widgets.Dropdown(options=[("steeper",(1.8,0.1)),("normal",(1,0.5)),("flatter",(0.2,0.9))],value=(1,0.5))
 
+    # b. create sliders for the parameters
     n_persons = widgets.IntSlider(25, 1, 50)
     n_firms = widgets.IntSlider(25, 1, 50)
     initial_p = widgets.IntSlider(75, 10, 90)
     initial_w = widgets.IntSlider(25, 10, 90)
     n_days = widgets.IntSlider(100, 50, 500, 10)
     
-    widgets.interact(update_x, p_dem=p_dem, p_sup=p_sup, l_dem=l_dem, l_sup=l_sup, n_persons=n_persons, n_firms=n_firms, initial_p=initial_p, initial_w=initial_w, n_days=n_days)
+    # c. make them interact within the simulation
+    widgets.interact(create_curves, p_dem=p_dem, p_sup=p_sup, l_dem=l_dem, l_sup=l_sup, n_persons=n_persons, n_firms=n_firms, initial_p=initial_p, initial_w=initial_w, n_days=n_days)
 
 
 
